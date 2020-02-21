e3995e65568b8298ba9882edf24563c0b149b3ba
CY OH <zxz1505@naver.com>
2020-02-21 16:29:01 +0900

    CEC Complete!

--- server.py
+++ server.py
@@ -6,29 +6,19 @@ import websockets
 import time  #delete
 import cec
 greeting = '\'contents of logcapture\''
-
+print(cec)
+print(websockets)
 async def echo(websocket,path):
-    global greeting    
+    global greeting
+    start_flag = 0
+    array = []
     print("\n",websocket)
     print(path)
     ms = '0'
     while 1:
-##################contents of logcapture###################
-        '''if ms == '11' :
-        elif ms == '12' :
-        elif ms == '13' :
-        elif ms == '21' :
-        elif ms == '22' :
-        elif ms == '23' :
-        elif len(ms) >= 3 : CecCommandTx(ms[3:])                #command tx 40:36...
-        else :'''
-#######################################################
-
 #test code
         time.sleep(0.1)
         await websocket.send(str(greeting))
-        #if greeting != '\'contents of logcapture\'':
-            #greeting = '\'contents of logcapture\''
         ms = await websocket.recv()
         print(ms)
         if(ms[0:4] == 'save') :
@@ -38,20 +28,38 @@ async def echo(websocket,path):
                 file = open(completeName, "w")
                 file.write(ms[ms.find(".txt") + 5:])
                 file.close()
+            cec.remove_callback(cb, cec.EVENT_ALL & ~cec.EVENT_LOG)
+            cec.remove_callback(log_cb, cec.EVENT_ALL)
             #else ignore
         elif(ms[0:5] == 'start') :
-            greeting = 'st'
-
+            if start_flag == 0 :
+                greeting = ''
+                cec.add_callback(cb, cec.EVENT_ALL & ~cec.EVENT_LOG)
+                cec.add_callback(log_cb, cec.EVENT_LOG)
+            start_flag = 1
+            cec.volume_up()
+            cec.volume_down()
+            cec.toggle_mute()
             
         elif(ms[0:4] == 'stop') :
-            greeting = 'st1'
-            
-        if(greeting == 'start') :
-            print("Start")
-            
-        elif(greeting == 'stop') :
-            print("Stop")
-            
+            cec.remove_callback(cb, cec.EVENT_ALL & ~cec.EVENT_LOG)
+            cec.remove_callback(log_cb, cec.EVENT_ALL)
+            start_flag = 0
+        elif(ms[0:2] == 'tx') :
+            print(len(ms))
+            print(ms[4])
+            print(ms[6:2])
+            #if(len(ms) <= 5) :
+            #    cec.transmit(int(ms[4]), a)
+            if(len(ms) <= 8) :
+                cec.transmit(int(ms[4]), int("0x" + ms[6:8], 16))
+            else :
+                array = []
+                n = 9
+                while(n < len(ms)) :
+                    array.append(int("0x" + ms[n:n+2] , 16))
+                    n += 3
+                cec.transmit(int(ms[4]), int("0x" + ms[6:8], 16), bytes(array))
 #echo에 logcapture내용을 실어서 HTML로 보냄
 #js에서 onmessage로 받아들여서 textarea로 띄우면 완료
 #save, create file 중 websocket communication이 안됨 (save create중 auto stop)
@@ -63,15 +71,12 @@ def cb(event, *args):
 def log_cb(event, level, time, message):
     print("CEC Log message:", message, "time:", time, "level:", level, "event:", event)
     global greeting
-    greeting += message + '\n'
-
-cec.add_callback(cb, cec.EVENT_ALL & ~cec.EVENT_LOG)
-cec.add_callback(log_cb, cec.EVENT_LOG)
-print("Callback added")
-time.sleep(2)
-
+    if(greeting == '\'contents of logcapture\'') :
+        greeting = ''
+    if(level == 8) :
+        greeting += message + "  [time : " + str(time) + ']\n'
 cec.init()
-
+print("ready")
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, '192.168.100.36', 8080)
 )
